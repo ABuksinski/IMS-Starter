20daaa1560e79634624df04ca9ca91b84865e643
ABuksinski <ABuksinski@qa.com>
2021-02-11 12:35:13 +0000

    IMS-23 #comment Adding SQL data to OrderItemTest

--- src/test/java/com/qa/ims/controllers/OrderItemControllerTest.java
+++ src/test/java/com/qa/ims/controllers/OrderItemControllerTest.java
@@ -5,11 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import com.qa.ims.controller.OrderItemController;
 import com.qa.ims.persistence.dao.OrderItemDAO;
